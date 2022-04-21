6245b5210d753eac271ea7cfc7a5e994161c3265
Long Phan <longpt08@gmail.com>
2022-04-22 04:24:31 +0700

    add new button

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -5,51 +5,52 @@
 
 <body>
 
-<!--*******************
-    Preloader start
-********************-->
-<div id="preloader">
-    <div class="loader">
-        <svg class="circular" viewBox="25 25 50 50">
-            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10"/>
-        </svg>
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="loader">
+            <svg class="circular" viewBox="25 25 50 50">
+                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10" />
+            </svg>
+        </div>
     </div>
-</div>
-<!--*******************
-    Preloader end
-********************-->
+    <!--*******************
+        Preloader end
+    ********************-->
 
 
-<!--**********************************
-    Main wrapper start
-***********************************-->
-<div id="main-wrapper">
+    <!--**********************************
+        Main wrapper start
+    ***********************************-->
+    <div id="main-wrapper">
 
 @include('admin.layout.nav-header')
-@include('admin.layout.header')
+    @include('admin.layout.header')
 @include('admin.layout.sidebar')
 
-<!--**********************************
+        <!--**********************************
             Content body start
         ***********************************-->
-    <div class="content-body">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <button type="button" class="btn mb-1 btn-info">Add new <span class="btn-icon-right"><i
-                                        class="fa fa-plus"></i></span>
+        <div class="content-body">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <button type="button" class="btn mb-1 btn-info">Add new <span class="btn-icon-right"><i
+                                            class="fa fa-plus"></i></span>
+                                </button>
                                 <div class="table-responsive">
                                     <table class="table table-bordered verticle-middle">
                                         <thead>
-                                        <tr>
-                                            <th scope="col">UID</th>
-                                            <th scope="col">Name</th>
-                                            <th scope="col">Description</th>
-                                            <th scope="col">Status</th>
-                                            <th scope="col">Action</th>
-                                        </tr>
+                                            <tr>
+                                                <th scope="col">UID</th>
+                                                <th scope="col">Name</th>
+                                                <th scope="col">Description</th>
+                                                <th scope="col">Status</th>
+                                                <th scope="col">Action</th>
+                                            </tr>
                                         </thead>
                                         <tbody>
                                         @foreach($categories as $category)
@@ -58,34 +59,29 @@
                                                 <td>{{$category->name}}</td>
                                                 <td>{{$category->description}}</td>
                                                 <td>{{\App\Http\Enums\CategoryStatus::convert($category->status)}}</td>
-                                                <td><span><a href="/admin/category/detail/{{$category->id}}"
-                                                             data-toggle="tooltip" data-placement="top" title="Edit"><i
-                                                                class="fa fa-pencil color-muted m-r-5"></i> </a><a
-                                                            href="#" data-toggle="tooltip" data-placement="top"
-                                                            title="Close"><i
-                                                                class="fa fa-close color-danger"></i></a></span></td>
+                                                <td><span><a href="/admin/category/detail/{{$category->id}}" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-pencil color-muted m-r-5"></i> </a><a href="#" data-toggle="tooltip" data-placement="top" title="Close"><i class="fa fa-close color-danger"></i></a></span></td>
                                             </tr>
                                         @endforeach
                                         </tbody>
                                     </table>
                                 </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <!-- #/ container -->
         </div>
-        <!-- #/ container -->
+        <!--**********************************
+            Content body end
+        ***********************************-->
+
+
+@include('admin.layout.footer')
     </div>
     <!--**********************************
-        Content body end
+        Main wrapper end
     ***********************************-->
-
-
-    @include('admin.layout.footer')
-</div>
-<!--**********************************
-    Main wrapper end
-***********************************-->
 
 @include('admin.layout.include-bottom')
 
